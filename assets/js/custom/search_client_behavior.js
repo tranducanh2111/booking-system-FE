0bf0e7363658a60722b37e46fc2d1501d85c6d06
Tran Duc Anh <tranducanh@sace.edu.vn>
2024-07-16 16:31:22 +1000

    fix page redirect logic of page/search_client

--- assets/js/custom/search_client_behavior.js
+++ assets/js/custom/search_client_behavior.js
@@ -1,8 +1,8 @@
 // Handle button clicks
-document.getElementById('continueAsGuestBtn').addEventListener('click', function() {
-    window.location.href = 'search_client.html'; // Redirect to search_client.html for sign in
+document.getElementById('signInBtn').addEventListener('click', function() {
+    window.location.href = 'search_client.html?guest=false'; // Redirect to search_client.html for sign in
 });
 
-document.getElementById('signInBtn').addEventListener('click', function() {
+document.getElementById('continueAsGuestBtn').addEventListener('click', function() {
     window.location.href = 'search_client.html?guest=true'; // Redirect to search_client.html for guest
 });
