581c845c182bf7dfee9c7e52505e47905479924e
Tran Duc Anh <tranducanh@sace.edu.vn>
2024-07-30 11:17:54 +1000

    dump the search_client_behavior logic, redesign page/search_client

--- assets/js/custom/fetch_current_client.js
+++ assets/js/custom/fetch_current_client.js
@@ -1,4 +1,4 @@
-// Get the modal
+// Get the overlay
 var overlay = document.getElementById("overlay");
 
 // Get the button that opens the modal
@@ -7,12 +7,34 @@ var btn = document.querySelector("#openPopup");
 // Get the cancel button inside the popup
 var cancelBtn = document.querySelector("#cancelBtn");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+// Function to show the overlay and dim other elements
+function showOverlay() {
     overlay.style.display = "block";
+    
+    // Select all child elements of <main> except #overlay
+    var mainChildren = document.querySelectorAll("main > *");
+    mainChildren.forEach(function(child) {
+        if (child !== overlay) {
+            child.style.opacity = "0.5";
+        }
+    });
 }
 
-// When the user clicks the cancel button, close the popup
-cancelBtn.onclick = function() {
+// Function to hide the overlay and restore opacity
+function hideOverlay() {
     overlay.style.display = "none";
+    
+    // Restore opacity for all child elements of <main>
+    var mainChildren = document.querySelectorAll("main > *");
+    mainChildren.forEach(function(child) {
+        if (child !== overlay) {
+            child.style.opacity = "1";
+        }
+    });
 }
+
+// When the user clicks the button, open the modal
+btn.onclick = showOverlay;
+
+// When the user clicks the cancel button, close the popup
+cancelBtn.onclick = hideOverlay;
